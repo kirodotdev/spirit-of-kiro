fd41d99ae9fa0fe9e96517a47aceb889cba41551
Nathan Peck <nathan.peck1@gmail.com>
2025-05-22 10:06:00 +1200

    Adding more crafting materials

--- server/llm/word-lists/index.js
+++ server/llm/word-lists/index.js
@@ -1,9 +1,13 @@
 import accessories from './accessories';
+import adhesives from './adhesives';
 import carParts from './car-parts';
 import clothing from './clothing';
 import containers from './containers';
 import cookingItems from './cooking-items';
+import foodIngredients from './food-ingredients';
 import householdItems from './household-items';
+import liquids from './liquids';
+import magical from './magical';
 import tools from './tools';
 import toys from './toys';
 import themes from './themes';
@@ -11,11 +15,15 @@ import themes from './themes';
 var allItems = [];
 
 allItems = allItems.concat(accessories);
+allItems = allItems.concat(adhesives);
 //allItems = allItems.concat(carParts);
 allItems = allItems.concat(clothing);
 allItems = allItems.concat(containers);
 allItems = allItems.concat(cookingItems);
+allItems = allItems.concat(foodIngredients);
 allItems = allItems.concat(householdItems);
+allItems = allItems.concat(liquids);
+allItems = allItems.concat(magical);
 allItems = allItems.concat(tools);
 allItems = allItems.concat(toys);
 
